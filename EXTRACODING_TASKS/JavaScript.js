a5b4aa9dd80fb66a5356051a658b7095ac6624a7
smokvyna02 <olenasmokvyna@gmail.com>
2022-01-08 19:09:06 -0800

    O(n^2)

---
+++ EXTRACODING_TASKS/JavaScript.js
@@ -0,0 +1,18 @@
+// To find to duplicate numbers in the array O(n):
+const arr = [2, 8, 3.5, -4, 20, 38, 20, 0];
+const duplicates = arr.filter(el => arr.indexOf(el) !== arr.lastIndexOf(el));
+console.log(duplicates[0]);
+
+// To find to duplicate numbers in the array O(n^2):
+function findNumber (array){
+    for (let i = 0; i < array.length; i++){
+        for (let j = i + 1; j < array.length; j++){
+            if (array[i] == array[j]){
+                return array[i];
+            }
+        }
+    }
+}
+
+const array = [...arr]; //... - метод распаковать array
+console.log(findNumber(array));
