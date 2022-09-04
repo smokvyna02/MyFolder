7d5e20bb7a98e11122414ce6dedbc8123f6c6d83
smokvyna02 <olenasmokvyna@gmail.com>
2022-09-04 13:58:46 -0700

    changes

--- EXTRACODING_TASKS/palindrome.js
+++ EXTRACODING_TASKS/palindrome.js
@@ -1,8 +1,29 @@
-function palindrome(string){
-    let str = '';
-    for (let i = string.length - 1; i >= 0; i--){
-        str += string[i];
+function palindrome(string1){
+    let str1 = '';
+    for (let i = string1.length - 1; i >= 0; i--){
+        str1 += string1[i];
     }
-    return str;
+    return str1;
 }
-console.log(palindrome("palindrome"))
+console.log(palindrome("palindrome"));
+
+let str2 = "was it a car or a cat i saw";
+function isPalindrome(str){
+    let reverseStr = "";
+    let strWithoutSpaces = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        if (str[i] != " "){
+            reverseStr += str[i];
+        }
+    }
+    for (let i = 0; i < str.length; i++){
+        if (str[i] != " "){
+            strWithoutSpaces += str[i];
+        }
+    }
+    if (reverseStr === strWithoutSpaces){
+        return true;
+    }
+    return false;
+}
+console.log(isPalindrome(str2));
