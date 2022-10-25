de50542f9a7f592fede4a7faf0a5d7e6691e08db
smokvyna02 <olenasmokvyna@gmail.com>
2022-10-24 23:01:09 -0700

    Count_Odd_Numbers_below_n

---
+++ CODEWARS/8SKUY/Count_Odd_Numbers_below_n.js
@@ -0,0 +1,14 @@
+// Given a number n, return the number of positive odd numbers below n
+function oddCount(n){
+    return Math.floor(n/2)
+  }
+
+//   function oddCount(n){
+//     x = []
+//     for (let i = 0; i < n; i++){
+//       if (i < n && i % 2 !== 0){
+//         x.push(i)
+//       }
+//     }
+//     return x.length
+//   }
